bb04cbbe0cd8264168c7788b75af377ba1ec757a
ucarfurkan <furkanucar011@gmail.com>
2023-04-01 22:26:10 +0300

    data.js is created

--- src/components/Spotlight.jsx
+++ src/components/Spotlight.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import { BsCheckCircleFill } from "react-icons/bs"
 import { FaPlay, FaInfo } from 'react-icons/fa';
+import { data } from '../data';
 
 
 function Spotlight() {
@@ -20,78 +21,45 @@ function Spotlight() {
         return () => window.removeEventListener("resize", handleResize);
     }, []);
 
+    function getSpotlights() {
+        const spotlightData = data.filter(x => x.isSpotlight);
+
+        return spotlightData.map(x => (
+            <Carousel.Item key={x.title}>
+                <div className='poster-container'>
+                    {!isMobile && (
+                        <>
+                            <div className='item-desc'>
+                                <div className='item-title'>{x.title}</div><br />
+                                <div className='iw-prime'><BsCheckCircleFill fill="cyan" />  Included with Prime</div>
+                                <div className='item-button-group d-flex align-items-center'>
+                                    <div className='play-button'><FaPlay /></div>
+                                    <div className='play-text'>Play</div>
+                                    <div className='watchlist-button'>+</div>
+                                    <div className='details-button'><FaInfo /></div>
+                                </div>
+                            </div>
+                            <div className="overlay"></div>
+                        </>
+                    )}
+                    <img
+                        className="d-block w-100 poster"
+                        src={x.spotlightImgPath}
+                        alt={x.title}
+                    />
+                </div>
+            </Carousel.Item>
+        ));
+    }
+
+
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
 
     return (
         <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <div className='poster-container'>
-                    {!isMobile && <>
-                        <div className='item-desc'>
-                            <div className='item-title'>THE BOYS</div><br />
-                            <div className='iw-prime'><BsCheckCircleFill fill="cyan" />  Included with Prime</div>
-                            <div className='item-button-group d-flex align-items-center'>
-                                <div className='play-button'><FaPlay /></div>
-                                <div className='play-text'>Play</div>
-                                <div className='watchlist-button'>+</div>
-                                <div className='details-button'><FaInfo /></div>
-                            </div>
-                        </div>
-                        <div className="overlay"></div>
-                    </>}
-                    <img
-                        className="d-block w-100 poster"
-                        src="./images/The Boys.jpg"
-                        alt="First slide"
-                    />
-                </div>
-            </Carousel.Item>
-            <Carousel.Item>
-                <div className='poster-container'>
-                    {!isMobile && <>
-                        <div className='item-desc'>
-                            <div className='item-title'>Ghostbusters</div><br />
-                            <div className='iw-prime'><BsCheckCircleFill fill="cyan" />  Included with Prime</div>
-                            <div className='item-button-group d-flex align-items-center'>
-                                <div className='play-button'><FaPlay /></div>
-                                <div className='play-text'>Play</div>
-                                <div className='watchlist-button'>+</div>
-                                <div className='details-button'><FaInfo /></div>
-                            </div>
-                        </div>
-                        <div className="overlay"></div>
-                    </>}
-                    <img
-                        className="d-block w-100 poster"
-                        src="./images/Ghostbusters.jpg"
-                        alt="First slide"
-                    />
-                </div>
-            </Carousel.Item>
-            <Carousel.Item>
-                <div className='poster-container'>
-                    {!isMobile && <>
-                        <div className='item-desc'>
-                            <div className='item-title'>The Marvelous Mrs. Maisel</div><br />
-                            <div className='iw-prime'><BsCheckCircleFill fill="cyan" />  Included with Prime</div>
-                            <div className='item-button-group d-flex align-items-center'>
-                                <div className='play-button'><FaPlay /></div>
-                                <div className='play-text'>Play</div>
-                                <div className='watchlist-button'>+</div>
-                                <div className='details-button'><FaInfo /></div>
-                            </div>
-                        </div>
-                        <div className="overlay"></div>
-                    </>}
-                    <img
-                        className="d-block w-100 poster"
-                        src="./images/The Marvelous Mrs. Maisel.jpg"
-                        alt="First slide"
-                    />
-                </div>
-            </Carousel.Item>
+            {getSpotlights()}
         </Carousel>
     );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = [
+    {
+        title: "The Boys",
+        isSpotlight: true,
+        spotlightImgPath: "./images/The Boys.jpg"
+    },
+    {
+        title: "Ghostbusters",
+        isSpotlight: true,
+        spotlightImgPath: "./images/Ghostbusters.jpg"
+    },
+    {
+        title: "The Marvelous Mrs. Maisel",
+        isSpotlight: true,
+        spotlightImgPath: "./images/The Marvelous Mrs. Maisel.jpg"
+    },
+    {
+        title: "Weekend",
+        isSpotlight: false,
+        spotlightImgPath: "./images/The Marvelous Mrs. Maisel.jpg"
+    },
+]
