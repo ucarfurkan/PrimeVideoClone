44a860c3364c57a6c5547f5928686cb6d972a28b
ucarfurkan <furkanucar011@gmail.com>
2023-04-03 16:27:14 +0300

    next-prev button bug fixed

--- src/components/DesktopMovies.jsx
+++ src/components/DesktopMovies.jsx
@@ -1,62 +1,57 @@
-import { useState } from "react"
-import { Carousel, Row, Col } from 'react-bootstrap';
+import { useState } from "react";
+import { Carousel, Row, Col } from "react-bootstrap";
 
 function DesktopMovies(props) {
-    const data = props.datas;
-    console.log(data);
-    const images = data.map(item => item.spotlightImgPath);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [itemsPerSlide, setItemsPerSlide] = useState(5);
+  const data = props.datas;
+  const images = data.map((item) => item.spotlightImgPath);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [itemsPerSlide, setItemsPerSlide] = useState(5);
 
-    const items = images.reduce((resultArray, item, index) => {
-        const chunkIndex = Math.floor(index / itemsPerSlide);
+  const items = images.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index / itemsPerSlide);
 
-        if (!resultArray[chunkIndex]) {
-            resultArray[chunkIndex] = [];
+    if (!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [];
+    }
+
+    resultArray[chunkIndex].push(item);
+
+    return resultArray;
+  }, []);
+
+  const handleSlide = (selectedIndex) => {
+    setCurrentIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel
+      interval={null}
+      className="movie-carousel"
+      controls={items.length > 1}
+      activeIndex={currentIndex}
+      onSelect={handleSlide}
+      wrap={false}
+    >
+      {[...Array(Math.ceil(images.length / itemsPerSlide))].map(
+        (group, groupIndex) => {
+          const start = groupIndex * itemsPerSlide;
+          const end = start + itemsPerSlide;
+          const groupItems = images.slice(start, end);
+          return (
+            <Carousel.Item key={groupIndex}>
+              <Row className="justify-content-center movie-row">
+                {groupItems.map((image, index) => (
+                  <Col md={2} key={start + index} className="movie-col">
+                    <img src={image} alt={`Image ${start + index + 1}`} />
+                  </Col>
+                ))}
+              </Row>
+            </Carousel.Item>
+          );
         }
-
-        resultArray[chunkIndex].push(item);
-
-        return resultArray;
-    }, []);
-
-    const handleSlide = (selectedIndex) => {
-        setCurrentIndex(selectedIndex);
-    };
-
-    return (
-        <Carousel
-            interval={null}
-            className="movie-carousel d-flex align-items-center"
-            controls={items.length > 1}
-            activeIndex={currentIndex}
-            onSelect={handleSlide}>
-                
-            {items.map((group, index) => (
-                <Carousel.Item key={index}>
-                    <Row className="justify-content-center movie-row">
-                        {group.map((image, index) => (
-                            <Col md={2} key={index} className="movie-col">
-                                <img
-                                    src={image}
-                                    alt={`Image ${index + 1}`}
-                                />
-                            </Col>
-                        ))}
-                    </Row>
-                </Carousel.Item>
-            ))}
-            <style type="text/css">
-                {`.movie-carousel .carousel-control-prev {
-                display: ${currentIndex === 0 ? "none" : ""};
-              }
-              .movie-carousel .carousel-control-next {
-                display: ${currentIndex === items.length - 1 ? "none" : ""
-                    };
-              }`}
-            </style>
-        </Carousel>
-    )
+      )}
+    </Carousel>
+  );
 }
 
 export default DesktopMovies;
