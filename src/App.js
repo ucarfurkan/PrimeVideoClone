24b0fee990a1bedac31695e30359554555105e36
ucarfurkan <furkanucar011@gmail.com>
2023-04-19 15:38:08 +0300

    corrected way of data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
+import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Main from './components/Main';
 import MovieDetails from "./components/MovieDetails";
 import Layout from "./components/Layout"
 
 function App() {
+  const [datas, setDatas] = useState([]);
+
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => setDatas(data.data));
+  }, []);
+
   return (
     (
+
       <>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Layout />}>
-              <Route index element={<Main />} />
+              <Route index element={<Main datas={datas} />} />
               <Route path="content">
-                <Route path=":contentName" element={<MovieDetails/>} />
+                <Route path=":contentName" element={<MovieDetails />} />
               </Route>
             </Route>
           </Routes>
         </BrowserRouter>
-
       </>
     )
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,15 +2,8 @@ import { useState, useEffect } from "react"
 import Spotlight from './Spotlight';
 import Movies from './Movies';
 
-function Main() {
-  const [datas, setDatas] = useState([]);
-
-
-  useEffect(() => {
-    fetch('./data.json')
-      .then(response => response.json())
-      .then(data => setDatas(data.data));
-  }, []);
+function Main(props) {
+const {datas} = props;
 
   return (
     (
